e8006e0293b925bd2090f336e628cf64c6dba2fc
MiguelPraw <miguelperezsebastian@hotmail.com>
2022-03-01 14:22:26 +0100

    Miguel
    Añadido funcionalidad basica del listado

--- pages/listado/funct.js
+++ pages/listado/funct.js
@@ -1,14 +1,13 @@
 
 let urls = {
     url_img: "https://api.thecatapi.com/v1/images/search",
-    url_fav: "https://api.thecatapi.com/v1/favourites",
-    url_voto: "https://api.thecatapi.com/v1/votes",
     url_breed: "https://api.thecatapi.com/v1/breeds",
     url_categoria: "https://api.thecatapi.com/v1/categories",
     estado: "gato"
 };
 
 let listaRazas = [];
+let listaCategorias;
 
 const URL_GATO = "api.thecatapi.com";
 const URL_PERRO = "api.thedogapi.com";
@@ -17,36 +16,121 @@ export function listadomain (){
 
     console.log("LISTADO");
 
-    getCategories (urls.url_categoria).then( datos => {
-        console.log(datos);
-        generaSelectCategoria ( datos );
-    });
-
-    getBreeds (urls.url_breed).then ( datos => {
-        console.log(datos);
-        generaSelectRaza ( datos );
-        console.log(listaRazas);
-        getImagenes (urls.url_img, listaRazas[4].id).then ( datos => {
-            console.log(datos);
-            pintaImagenes (datos);
-        }).catch ( error => {
-            console.log(error);
-        });
-    });
+    inicializaListado ();
 
     $(document).ready( function () {
 
+        $('.btn__swap').on({
+            click: function () {
+                if (this.classList.contains('swapOn')) {
+                    this.classList.remove('swapOn');
+                } else {
+                    this.classList.add('swapOn');
+                }
+                cambiaEstado ();
+                $('#raza').empty();
+                $('#categoria').empty();
+                inicializaListado ();
+            }
+        });
+
         $('#raza').on({
             change: function () {
-                console.log($(this).val());
+                filtraImagenes ();
+            }
+        });
+
+        $('#categoria').on({
+            change: function () {
+                filtraImagenes ();
             }
         });
     });
     
 }
 
+function inicializaListado () {
+    desactivaBotones ();
+    getCategories (urls.url_categoria).then( datos => {
+        listaCategorias = datos;
+        generaSelectCategoria ( datos );
+        getBreeds (urls.url_breed).then ( datos => {
+            generaSelectRaza ( datos );
+            filtraImagenes ();
+        });
+    }).catch ( error  => {
+        console.log ( error );
+    });
+}
+
+function cambiaEstado () {
+
+    let nodoSpanGatos = document.querySelector('.swap__gatos');
+    let nodoSpanPerros = document.querySelector('.swap__perros');
+
+    let urlImg = urls.url_img.split("/");
+    let urlBreed = urls.url_breed.split("/");
+    let urlCategoria = urls.url_categoria.split("/");
+
+    if (urls.estado === "gato") {
+        nodoSpanGatos.classList.remove('activo');
+        urls.estado = "perro";
+        urlImg[2] = URL_PERRO;
+        urlBreed[2] = URL_PERRO;
+        urlCategoria[2] = URL_PERRO;
+        nodoSpanPerros.classList.add('activo');
+    } else {
+        nodoSpanPerros.classList.remove('activo');
+        urls.estado = "gato";
+        urlImg[2] = URL_GATO;
+        urlBreed[2] = URL_GATO;
+        urlCategoria[2] = URL_GATO;
+        nodoSpanGatos.classList.add('activo');
+    }
+
+    urlImg = urlImg.join("/");
+    urlBreed = urlBreed.join("/");
+    urlCategoria = urlCategoria.join("/");
+    
+    urls.url_img = urlImg;
+    urls.url_breed = urlBreed;
+    urls.url_categoria = urlCategoria;
+
+}
+
+function desactivaBotones () {
+    $('#raza')[0].disabled = true;
+    $('#categoria')[0].disabled = true;
+}
+
+function activaBotones () {
+    $('#raza')[0].disabled = false;
+    $('#categoria')[0].disabled = false;
+}
+
+function devuelveIdRaza (nombre) {
+    if (nombre === "Todas") {
+        return "Todas";
+    } else {
+        return listaRazas.find( elemento => elemento.nombre === nombre ).id;
+    }
+}
+
+function devuelveIdCategoria (categoria) {
+    if (categoria === "Todas") {
+        return "Todas";
+    } else {
+        return listaCategorias.find ( elemento => elemento.name === categoria ).id;
+    }
+}
+
 function generaSelectRaza (datos) {
     let nodoSelect = document.querySelector('#raza');
+    let nodoOptionInicial = document.createElement('option');
+    nodoOptionInicial.value = "Todas";
+    nodoOptionInicial.innerHTML = "Todas";
+    nodoOptionInicial.selected = true;
+    nodoSelect.appendChild(nodoOptionInicial);
     datos.forEach( raza => {
         let objectRaza = {
             id: raza.id,
@@ -62,6 +146,11 @@ function generaSelectRaza (datos) {
 
 function generaSelectCategoria (datos) {
     let nodoSelect = document.querySelector('#categoria');
+    let nodoOptionInicial = document.createElement('option');
+    nodoOptionInicial.value = "Todas";
+    nodoOptionInicial.innerHTML = "Todas";
+    nodoOptionInicial.selected = true;
+    nodoSelect.appendChild(nodoOptionInicial);
     datos.forEach( categoria => {
         let nodoOption = document.createElement('option');
         nodoOption.value = categoria.name;
@@ -70,44 +159,22 @@ function generaSelectCategoria (datos) {
     });
 }
 
-function cambiaEstado () {
-
-    let nodoSpanGatos = document.querySelector('.swap__gatos');
-    let nodoSpanPerros = document.querySelector('.swap__perros');
-
-    let urlImg = urls.url_img.split("/");
-    let urlFav = urls.url_fav.split("/");
-    let urlVoto = urls.url_voto.split("/");
-
-    if (urls.estado === "gato") {
-        nodoSpanGatos.classList.remove('activo');
-        console.log(nodoSpanGatos);
-        urls.estado = "perro";
-        urlImg[2] = URL_PERRO;
-        urlFav[2] = URL_PERRO;
-        urlVoto[2] = URL_PERRO;
-        nodoSpanPerros.classList.add('activo');
-    } else {
-        nodoSpanPerros.classList.remove('activo');
-        urls.estado = "gato";
-        urlImg[2] = URL_GATO;
-        urlFav[2] = URL_GATO;
-        urlVoto[2] = URL_GATO;
-        nodoSpanGatos.classList.add('activo');
-    }
-    urlImg = urlImg.join("/");
-    urlFav = urlFav.join("/");
-    urlVoto = urlVoto.join("/");
-    
-    urls.url_img = urlImg;
-    urls.url_fav = urlFav;
-    urls.url_voto = urlVoto;
-
+function filtraImagenes () {
+    let idRaza = devuelveIdRaza ($('#raza').val());
+    let idCategoria = devuelveIdCategoria ($('#categoria').val());
+    desactivaBotones();
+    $('.listado__imagenes').empty();
+    getImagenes (urls.url_img, idCategoria, idRaza).then ( datos => {
+        pintaImagenes ( datos );
+        activaBotones ();
+    }).catch ( error => {
+        console.log ( error );
+    });
 }
 
-function getImagenes (url, idRaza) {
-    return new Promise ( (resolve, reject ) => {
-        let urlRequest = url + "?limit=20&breed_id=" + idRaza;
+function getImagenes (url, idCategoria, idRaza) {
+    return new Promise ( (resolve, reject) => {
+        let urlRequest = devuelveURL (url, idCategoria, idRaza);
         fetch (urlRequest, {
             method: "GET",
             headers: {
@@ -122,6 +189,18 @@ function getImagenes (url, idRaza) {
             reject ( error );
         });
     });
+}
+
+function devuelveURL (url, idCategoria, idRaza) {
+    if (idCategoria !== "Todas" && idRaza !== "Todas") {
+        return `${url}?limit=20&breed_id=${idRaza}&category_ids=${idCategoria}`;
+    } else if (idCategoria === "Todas" && idRaza !== "Todas") {
+        return `${url}?limit=20&breed_id=${idRaza}`;
+    } else if (idCategoria !== "Todas" && idRaza === "Todas") {
+        return `${url}?limit=20&category_ids=${idCategoria}`;
+    } else {
+        return `${url}?limit=20`;
+    }
 }
 
 function getBreeds (url) {
@@ -179,11 +258,3 @@ function devuelveNodoImg (src) {
     nodoImg.classList.add('imagen');
     return nodoImg;
 }
-
-$(window).on({
-    scroll: function () {
-        console.log($(this).height());
-        console.log($(this).scrollTop());
-    }
-});
-
